ae58b5d89922fe5329fb6444affb55e98513bacf
Emilio <42880163+emiliodominguez@users.noreply.github.com>
2020-08-10 12:17:18 -0300

    Added drag and drop setup

--- src/components/FileInput/FileInput.jsx
+++ src/components/FileInput/FileInput.jsx
@@ -1,35 +1,62 @@
-import React from "react";
+import React, { useRef } from "react";
+import useEventListener from "../../hooks/useEventListener";
 import { imageIcon } from "../../constants/icons";
 import styles from "./FileInput.module.scss";
 
+const SUPPORTED_TYPES = [
+    // "image/gif",
+    "image/apng",
+    "image/bmp",
+    "image/jpeg",
+    "image/jpg",
+    "image/pjpeg",
+    "image/png",
+    "image/tiff",
+    "image/webp"
+];
+
+const stylesState = {
+    add: "ADD",
+    remove: "REMOVE"
+};
+
 const FileInput = ({ noImages, setImages }) => {
-    const fileTypes = [
-        // "image/gif",
-        "image/apng",
-        "image/bmp",
-        "image/jpeg",
-        "image/jpg",
-        "image/pjpeg",
-        "image/png",
-        "image/tiff",
-        "image/webp"
-    ];
+    const input = useRef();
 
     const handleInputChange = ({ target }) => {
         setImages([...target.files]);
         target.value = null;
     };
 
+    const toggleInputStyles = (e, state, isDrop = false) => {
+        if (!isDrop) e.preventDefault();
+        e.stopPropagation();
+
+        const { parentElement } = input.current;
+
+        if (state === stylesState.add) parentElement.classList.add(styles.isActive);
+        if (state === stylesState.remove) parentElement.classList.remove(styles.isActive);
+    };
+
+    useEventListener("dragover", e => toggleInputStyles(e, stylesState.add), input.current);
+    useEventListener("dragleave", e => toggleInputStyles(e, stylesState.remove), input.current);
+    useEventListener("drop", e => toggleInputStyles(e, stylesState.remove, true), input.current);
+
     return (
         <div className={styles.fileInput}>
             {noImages && imageIcon}
 
             <p>
-                Click here to browse <b>images</b>
-                <small>Drag and Drop coming soon</small>
+                Drag your <b>images</b> to this box or click it to browse on your computer...
             </p>
 
-            <input type="file" accept={fileTypes.join(",")} multiple onChange={handleInputChange} />
+            <input
+                ref={input}
+                type="file"
+                accept={SUPPORTED_TYPES.join(",")}
+                multiple
+                onChange={handleInputChange}
+            />
         </div>
     );
 };
